0026032c0177dde21abfe82ffd4a20ab17f11c35
Ben G Vuong <78512101+BenVuong@users.noreply.github.com>
2024-10-24 18:02:33 -0500

    styled messeng component

--- src/lib/Messages.svelte
+++ src/lib/Messages.svelte
@@ -6,6 +6,7 @@
   import { Textarea } from "$lib/components/ui/textarea";
   let messages: any[] = [];
   let newMessage: string;
+  let messageContainer;
   let unsubscribe: () => void;
   pb.autoCancellation(false);
   onMount(async () => {
@@ -44,26 +45,30 @@
 </script>
 
 <Card.Root>
-  <Card.Header>
+  <Card.Header class="justify-items-center bg-gray-500">
     <Card.Title>Chat Room</Card.Title>
-    <Card.Description>Chat Room Description</Card.Description>
+    <Card.Description class="text-black"
+      >Enjoy talking with others</Card.Description
+    >
   </Card.Header>
   <Card.Content
-    class="h-80 overflow-y-auto scrollbar-hide bg-white border border-gray-200  shadow-inner"
+    class="h-80 max-w-lg flow-text break-words overflow-y-auto scrollbar-hide bg-white border-2 border-gray-500  shadow-inner"
   >
-    <div class="messages">
+    <div class="flow-text break-words">
       {#each messages as message (message.id)}
-        <div>
-          <div>
-            {message.expand?.user?.username} : {message.text}
-          </div>
+        <div class="py-1">
+          <small class="flow-text break-words">
+            {message.expand?.user?.username}
+            {"["}{message.created}{"]"}:
+          </small>
+          <div>{message.text}</div>
         </div>
       {/each}
     </div>
   </Card.Content>
-  <Card.Footer>
-    <form on:submit|preventDefault={sendMessage}>
-      <Textarea placeholder="Message" bind:value={newMessage} />
+  <Card.Footer class=" border-2 border-gray-500">
+    <form class="py-3" on:submit|preventDefault={sendMessage}>
+      <Textarea placeholder="Message" class=" w-80" bind:value={newMessage} />
       <Button class="bg-black text-white" variant="outline" type="submit"
         >Send</Button
       >
